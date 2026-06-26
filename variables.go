0672e32b19f33f1c9840e35f235f42e8ae231d4d
Prachi Gotal <prachigotalios@gmail.com>
2026-06-27 00:28:53 +0530

    variables

---
+++ variables.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	var name = "Joe"
+	fmt.Println(name)
+	var age int = 36
+	fmt.Println(age)
+	var is_male = true 
+	fmt.Println("is male ", is_male)
+	var total_cars_owned int
+	fmt.Println("Total cars owned ", total_cars_owned)
+	var gender, email = "male", "joe@joe.com"
+	fmt.Println("Gender and email are ",gender, email )
+	loves_apples := true 
+	fmt.Println("Loves apples ", loves_apples)
+
+
+}
