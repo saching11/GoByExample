d9ec22faa21e716a5521ed08027de7c691b69500
Prachi Gotal <prachigotalios@gmail.com>
2026-06-27 00:21:29 +0530

    values

---
+++ values.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("This is a " + "String concatenation")
+	fmt.Println("1+1 = ", 1+1)
+	fmt.Println("7.0/3.0 = ", 7.0/3.0)
+	fmt.Println("true and false = ", true && false)
+	fmt.Println("true or false = ", true || false)
+	fmt.Println("Not true = ", !true)
+}
